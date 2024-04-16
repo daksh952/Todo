964569179285b9ef9a518c53873027175aec2436
daksh952 <dakshpatidar952@gmail.com>
2024-04-16 10:39:51 +0530

    First

---
+++ src/Components/ListHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ListHeader({listName}) {
+  
+  return (
+    <div className='list-header'> 
+      <h1>{listName}</h1> 
+    </div>
+  )
+}
+
+export default ListHeader
